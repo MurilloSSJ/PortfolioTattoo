9e54241a8b1f5966362e34ebffa129d8f6868f13
MurilloSSJ <murillostore@gmail.com>
2021-11-21 13:59:08 -0300

    Responsible

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -13,12 +13,3 @@ closeMenu.addEventListener('click',()=>{
     menu.classList.remove("menuActive")
     toggleMenu.classList.add('menuActive')
 })
-
-let homeSwiper = new Swiper(".home-swiper", {
-    spaceBetween: 30,
-    loop: 'true',
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-      },
-})
